aab55c405e7ed5ca772f2281c9bdbbd3fdd213b4
jksun <sjkyspa@gmail.com>
2015-11-11 17:20:57 +0800

    update build mesos config

--- buildmesos/build.sh
+++ buildmesos/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-curl -X POST http://54.251.190.73:8080/v2/apps -d @umsbuild.json -H "Content-type: application/json"
+curl -X POST http://54.254.139.175:8080/v2/apps -d @umsbuild.json -H "Content-type: application/json"
 
