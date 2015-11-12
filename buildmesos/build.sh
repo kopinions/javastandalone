0d2b49d935f049e5ce9059d0b1c6b03984bfb6db
jksun <sjkyspa@gmail.com>
2015-11-12 18:08:11 +0800

    update config

--- buildmesos/build.sh
+++ buildmesos/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-curl -X POST http://54.254.139.175:8080/v2/apps -d @umsbuild.json -H "Content-type: application/json"
+curl -X POST http://52.76.134.79:8080/v2/apps -d @umsbuild.json -H "Content-type: application/json"
 
--- run/run.sh
+++ run/run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 #curl -X POST http://54.254.139.175:8080/v2/apps -d @dockerregistry.json -H "Content-type: application/json"
-curl -X POST http://54.254.139.175:8080/v2/apps -d @ums.json -H "Content-type: application/json"
+
+curl -X POST http://52.76.134.79:8080/v2/apps -d @ums.json -H "Content-type: application/json"
 
