98a7f04a2809b5c1990728c5cc41780310305293
jksun <sjkyspa@gmail.com>
2015-11-13 17:46:21 +0800

    add runtime

--- captcha/src/main/java/com/thoughtworks/captcha/CaptchaResource.java
+++ captcha/src/main/java/com/thoughtworks/captcha/CaptchaResource.java
@@ -1,13 +1,27 @@
 package com.thoughtworks.captcha;
 
+import com.thoughtworks.captcha.util.DbUtil;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
+import java.sql.*;
 
 @Path("captcha")
 public class CaptchaResource {
     @GET
-    public Response captcha() {
-        return Response.ok().build();
+    public Response captcha() throws SQLException {
+        Connection connection = DbUtil.getConnection();
+        ResultSet catalogs = connection.getMetaData().getCatalogs();
+        StringBuilder builder = new StringBuilder();
+
+        while (catalogs.next()) {
+            builder.append("db name:\t")
+                    .append(catalogs.getString("TABLE_CAT"))
+                    .append("</br>");
+        }
+        catalogs.close();
+
+        return Response.ok(builder.toString()).build();
     }
 }
---
+++ captcha/src/main/java/com/thoughtworks/captcha/util/DbUtil.java
@@ -0,0 +1,30 @@
+package com.thoughtworks.captcha.util;
+
+import com.thoughtworks.cloud.CloudEnvironment;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Map;
+
+public class DbUtil {
+
+    private static Connection connection = null;
+
+    public static Connection getConnection() {
+        if (connection != null)
+            return connection;
+        else {
+            try {
+                CloudEnvironment env = new CloudEnvironment();
+                Map<String, Object> mysql = env.getServiceDataByName("mysql");
+                String uri = String.valueOf(mysql.get("uri"));
+                connection = DriverManager.getConnection(uri);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            return connection;
+        }
+
+    }
+}
