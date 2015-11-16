ba212bdff71a0877257c54300188880a67e199e5
jksun <sjkyspa@gmail.com>
2015-11-16 13:45:15 +0800

    update config

--- buildmesos/build.sh
+++ buildmesos/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-curl -X POST http://52.76.134.79:8080/v2/apps -d @umsbuild.json -H "Content-type: application/json"
+curl -X POST http://192.168.50.4:8080/v2/apps -d @umsbuild.json -H "Content-type: application/json"
 
